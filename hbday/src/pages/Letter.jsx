39ebc257e4792c264602265e3373fc0fb8ecd811
Jeslyn Kuai <jslynk@Jeslyns-MacBook-Air.local>
2026-08-17 20:50:31 -0700

    more updates

--- hbday/src/pages/Letter.jsx
+++ hbday/src/pages/Letter.jsx
@@ -12,6 +12,10 @@ function Letter() {
     <main className="letter-page">
       <p>oh!</p>
       <h1>You've received a letter!</h1>
+            
+      <button className="keep-looking-button" onClick={() => window.location.href = '/Gifts'}>
+        oo what else is there...
+      </button>
     </main>
   )
 }
